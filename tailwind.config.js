fff0422ede9f18879528c0c416fc0e53831d7234
Raymond Meibaum <15313325+raymeibaum@users.noreply.github.com>
2021-02-10 15:29:21 -0500

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: {
+      enabled: true,
+      content: ['./*.html'],
+    },
+    theme: {
+      extend: {},
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        display: ['Big Shoulders Display', 'cursive']
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
